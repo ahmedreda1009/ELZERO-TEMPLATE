74795e02cdb065192840b73448c9f7d8a246bef7
Ahmed Reda <ahmedreda1009@gmail.com>
2022-08-09 01:01:56 +0200

    added the testimonials section.

--- main.js
+++ main.js
@@ -10,14 +10,25 @@ toggleNav.addEventListener('click', () => {
     }
 });
 
-// const secondaryNav = document.querySelector('.mega-menu');
-// let secondaryOpen = false;
-// secondaryNav.addEventListener('click',() => {
-//     if(!secondaryOpen) {
-//         secondaryNav.classList.add('open');
-//         secondaryOpen = true;
-//     } else {
-//         secondaryNav.classList.remove('open');
-//         secondaryOpen = false;
-//     }
-// });
+let rating = document.querySelectorAll(".testimonials .rating");
+let ratingStar = document.querySelectorAll(".testimonials .rating i");
+rating.forEach(element => {
+    ratingStar.forEach(element => {
+        element.setAttribute("data-id",  performance.now().toString(36)+Math.random().toString(36).replace(/\./g,""));
+        element.onclick = (event) => {
+            element.classList.add("filled", "fas");
+            let siblings  = getPreviousSiblings(element);
+            siblings .forEach(element => {
+                element.classList.add("filled", "fas");
+            });
+        }
+    });
+});
+
+function getPreviousSiblings(element) {
+    var siblings  = [];
+    while (element = element.previousElementSibling) {
+        siblings.push(element);
+    }
+    return siblings ;
+}

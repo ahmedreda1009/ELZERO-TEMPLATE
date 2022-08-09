e66f3c5edee0b145e16d3f47be6cb77930517d28
Ahmed Reda <ahmedreda1009@gmail.com>
2022-08-09 21:42:09 +0200

    modified the stars rating of the testimonials sectio.

--- main.js
+++ main.js
@@ -10,17 +10,54 @@ toggleNav.addEventListener('click', () => {
     }
 });
 
+// rating stars
 let rating = document.querySelectorAll(".testimonials .rating");
 let ratingStar = document.querySelectorAll(".testimonials .rating i");
+
+let filledStars = [];
+
+// looping over each card rating stars
 rating.forEach(element => {
+    // looping over each element in one card
     ratingStar.forEach(element => {
-        element.setAttribute("data-id",  performance.now().toString(36)+Math.random().toString(36).replace(/\./g,""));
+        // when clicking the star
         element.onclick = (event) => {
-            element.classList.add("filled", "fas");
-            let siblings  = getPreviousSiblings(element);
-            siblings .forEach(element => {
+            // console.log(element.nextElementSibling)
+            if (!element.classList.contains("filled")) {
+                // color the element
                 element.classList.add("filled", "fas");
-            });
+                filledStars.push(element.dataset.id);
+                // color the previous elements
+                let prevSiblings  = getPreviousSiblings(element);
+                prevSiblings.forEach(element => {
+                    element.classList.add("filled", "fas");
+                    filledStars.push(element.dataset.id);
+                });
+                // make sure that the next elements is not colored
+                let nextSiblings = getNextSiblings(element);
+                nextSiblings.forEach(element => {
+                    element.classList.remove("filled", "fas");
+                    // console.log(element.dataset.id);
+                });
+            } else if (element.classList.contains("filled") && element.nextElementSibling.classList.contains("filled")) {
+                let nextSiblings = getNextSiblings(element);
+                nextSiblings.forEach(element => {
+                    element.classList.remove("filled", "fas");
+                });
+            } else {
+                // color the element
+                element.classList.remove("filled", "fas");
+                // color the previous elements
+                let prevSiblings  = getPreviousSiblings(element);
+                prevSiblings.forEach(element => {
+                    element.classList.remove("filled", "fas");
+                });
+                // make sure that the next elements is not colored
+                let nextSiblings = getNextSiblings(element);
+                nextSiblings.forEach(element => {
+                    element.classList.remove("filled", "fas");
+                });
+            }
         }
     });
 });
@@ -28,6 +65,14 @@ rating.forEach(element => {
 function getPreviousSiblings(element) {
     var siblings  = [];
     while (element = element.previousElementSibling) {
+        siblings.push(element);
+    }
+    return siblings ;
+}
+
+function getNextSiblings(element) {
+    var siblings  = [];
+    while (element = element.nextElementSibling) {
         siblings.push(element);
     }
     return siblings ;
